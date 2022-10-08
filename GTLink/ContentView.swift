e551737bd807b0269dd724cb1aa31933e91956aa
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-10-08 18:16:39 -0400

    Initial Commit

---
+++ GTLink/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GTLink
+//
+//  Created by Abdulaziz Albahar on 10/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GTLink/GTLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTLinkApp.swift
+//  GTLink
+//
+//  Created by Abdulaziz Albahar on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GTLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
