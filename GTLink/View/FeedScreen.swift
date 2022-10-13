978d7225e1e85a9987be805a6b07436668e5efad
Amit Kulkarni <akulkarni334@gatech.edu>
2022-10-13 17:44:42 -0400

    Organize files into folders

---
+++ GTLink/View/FeedScreen.swift
@@ -0,0 +1,20 @@
+//
+//  FeedScreen.swift
+//  GTLink
+//
+//  Created by Amit Kulkarni on 10/13/22.
+//
+
+import SwiftUI
+
+struct FeedScreen: View {
+    var body: some View {
+        Text("Feed")
+    }
+}
+
+struct FeedScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedScreen()
+    }
+}
