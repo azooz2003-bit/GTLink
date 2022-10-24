c635ed3bddec23b34f2c8a295fc899dddccd14d1
River Pease <riverpease15@gmail.com>
2022-10-24 16:02:19 -0400

    Basic UI (not functional) for the first of the three sign up views.

--- GTLink/ContentView.swift
+++ GTLink/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        // Welcome message
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Welcome, @gburdell!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 75.0)
+            
+            // Intro
+            VStack(alignment: .leading) {
+                Text("Let's start with the basics.")
+                    .font(.title)
+                (Text("*").foregroundColor(.red) + Text(" indicates a required field"))
+                    .fontWeight(.light)
+            }
+            
+            // Full name
+            VStack(alignment: .leading) {
+                (Text("Full Name ") +    Text("*").foregroundColor(.red))
+                    .font(.title3)
+                    .padding(.top, 30.0)
+                    .fontWeight(.light)
+                TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(height: 30)
+                    .overlay(RoundedRectangle(cornerRadius: 5) .stroke(Color.gray) )
+                    .padding(.top, -10)
+            }
+            .frame(width: 310)
+            
+            // Description
+            VStack(alignment: .leading) {
+                Text("Description")
+                    .font(.title3)
+                    .padding(.top, 30.0)
+                    .fontWeight(.light)
+                TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(height: 150)
+                    .overlay(RoundedRectangle(cornerRadius: 5) .stroke(Color.gray) )
+                    .padding(.top, -10)
+            }
+            .frame(width: 310)
+            .padding(.bottom, 100)
+            
+            // Next button
+            Button("Next") {
+                print("Next button clicked.")
+            }
+                .frame(width: 120, height: 50)
+                .foregroundColor(Color.black)
+                .overlay(RoundedRectangle(cornerRadius: 25) .stroke(Color.black) )
         }
-        .padding()
+        
     }
 }
 
