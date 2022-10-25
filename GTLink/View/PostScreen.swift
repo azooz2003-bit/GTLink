772a31a9739c0ba0322dfaf7f36a9a6a8f77c9d9
jryzhkov3 <jryzhkov3@gatech.edu>
2022-10-24 20:05:52 -0400

    Complete the Image view and "Add image button"

--- GTLink/View/PostScreen.swift
+++ GTLink/View/PostScreen.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+extension View {
+     public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+         let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+         return clipShape(roundedRect)
+              .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+     }
+ }
+
 struct PostScreen: View {
     var body: some View {
         NavigationView {
-            Text("Hello, SwiftUI!")
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color(red:0.882, green: 0.882, blue: 0.882))
+                        .frame(height: 200)
+                    
+                        Button(action: {
+                            print("button clicked")
+                        }) {
+                            HStack {
+                                Image(systemName: "photo")
+                                Text("Add Image").font(.system(size: 17, weight: .bold))
+                            }
+                            .padding(.leading,25)
+                            .padding(.bottom, 5)
+                            .padding(.top, 5)
+                            .padding(.trailing,25)
+                            .foregroundColor((Color(red:0.709, green: 0.709, blue: 0.709)))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(red:0.709, green: 0.709, blue: 0.709), lineWidth: 2)
+                            )
+                            .background(.white, in: RoundedRectangle(cornerRadius: 10))
+                        }
+
+//                    RoundedRectangle(cornerRadius: 10)
+//                        .background(RoundedRectangle(cornerRadius: 4.0).stroke(Color.red, lineWidth: 4))
+//                        .fill(Color(red:0.93, green: 0.93, blue: 0.93))
+//                        .frame(width: 181, height: 39)
+                }
+                Spacer()
+            }
                     .navigationBarTitleDisplayMode(.inline)
                     .toolbar {
                         ToolbarItem(placement: .principal) {
@@ -18,6 +57,7 @@ struct PostScreen: View {
                                 Image(systemName: "arrow.left")
                                     .foregroundColor(.black)
                                     .bold()
+                                    .scaleEffect(1.2)
                                 Spacer()
                                 Text("Create a Post").font(.system(size: 30, weight: .bold))
                                     .font(.largeTitle.bold())
@@ -26,6 +66,21 @@ struct PostScreen: View {
                             }
                         }
                     }
+        }
+        VStack {
+            Text("HEllo")
+        }
+
+    }
+}
+
+struct FeedScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PostScreen()
+    }
+}
+
+
 //            Text("Create a Post").font(.system(size: 30, weight: .bold))
 //            .navigationTitle("Create a Post")
 //            .toolbar {
@@ -46,13 +101,3 @@ struct PostScreen: View {
 //                        .bold()
 //                }
 //            })
-            
-        }
-    }
-}
-
-struct FeedScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        PostScreen()
-    }
-}
