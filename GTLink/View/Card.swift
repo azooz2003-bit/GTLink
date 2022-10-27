92c3fbb9c6d218f4fc069c8a2d1f143a1875d2ee
Nicholas Candello <nac5504@gmail.com>
2022-10-27 19:12:13 -0400

    Fully integrated card object with tag

--- GTLink/View/Card.swift
+++ GTLink/View/Card.swift
@@ -15,10 +15,10 @@ struct Card: View {
     let title: String
     let username: String
     let post_date: Date
-    let tags: [String]
+    let tags: [Tag]
     let description: String
     
-    init(post_picture: Image, title: String, username: String, post_date: Date, tags: [String], description: String) {
+    init(post_picture: Image, title: String, username: String, post_date: Date, tags: [Tag], description: String) {
         self.post_picture = post_picture
         self.title = title
         self.username = username
@@ -28,53 +28,59 @@ struct Card: View {
     }
     
     var body: some View {
-            //card view
-            VStack {
+        //card view
+        VStack {
+            
+            HStack(alignment: .top) {
                 
-                HStack(alignment: .top) {
+                //post image
+                post_picture
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 140, height: 140)
+                
+                //right side VStack
+                VStack(alignment: .leading, spacing: 14) {
                     
-                    //post image
-                    post_picture
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 140, height: 140)
                     
-                    //right side VStack
-                    VStack(alignment: .leading, spacing: 14) {
-                        
-                        
-                        Text(title)
-                            .font(.system(size: 16))
-                            .bold()
-                        
-                        //username and date
-                        HStack {
-                            Image(systemName: "person")
-                            Text(username)
+                    Text(title)
+                        .font(.system(size: 16))
+                        .bold()
+                    
+                    //username and date
+                    HStack {
+                        Image(systemName: "person")
+                        Text(username)
+                        Spacer()
+                        Image(systemName: "clock")
+                        Text(date_to_string(date:post_date))
+                    }.font(.system(size: 12))
+                        .padding(.trailing, 10)
+                    
+                    //tags list
+                    HStack {
+                        if(tags.count >= 3) {
                             Spacer()
-                            Image(systemName: "clock")
-                            Text(date_to_string(date:post_date))
-                        }.font(.system(size: 12))
-                            .padding(.trailing, 10)
-                        
-                        //tags list
-                            HStack {
-                                Text(tags[0]).padding(5).background(Color.red).clipShape(Capsule())
-                                Text(tags[1]).padding(5).background(Color.green).clipShape(Capsule())
-                                Text("+3").padding(5).background(Color.gray).clipShape(Capsule())
-                            }.font(.system(size: 12))
-                                .foregroundColor(Color.white)
-                        
-                        //description of post
-                        Text(description)
-                            .font(.system(size: 10))
-                            .truncationMode(.tail)
-                        
-                    }.padding(5)
-                }
-            }.frame(width: phone_size.width * 35/39, height: phone_size.height * 140/844)
-                .cornerRadius(15)
-                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.black, lineWidth: 1))
+                        }
+                        Text(tags[0].rawValue).padding(5).background(Color.red).clipShape(Capsule())
+                        Text(tags[1].rawValue).padding(5).background(Color.green).clipShape(Capsule())
+                        if(tags.count >= 3) {
+                            Text("+\(tags.count - 2)").padding(5).background(Color.gray).clipShape(Capsule())
+                        }
+                    }.font(.system(size: 10.7))
+                        .foregroundColor(Color.white)
+                        .padding(.trailing, 5)
+                    
+                    //description of post
+                    Text(description)
+                        .font(.system(size: 10))
+                        .truncationMode(.tail)
+                    
+                }.padding(5)
+            }
+        }.frame(width: phone_size.width * 35/39, height: phone_size.height * 140/844)
+            .cornerRadius(15)
+            .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.black, lineWidth: 1))
     }
     
     func date_to_string(date: Date) -> String {
@@ -86,6 +92,6 @@ struct Card: View {
 
 struct Card_Previews: PreviewProvider {
     static var previews: some View {
-        Card(post_picture: Image("penguin"), title: "CS 1999: Exam 1 Study", username: "gburdell", post_date: Date(), tags: ["CS 1999", "Class Project", "Homework"], description: "Lorem ipsum dolor sit amet, sed do eiusmod tempor quis nos vas de roma.")
+        Card(post_picture: Image("penguin"), title: "CS 1999: Exam 1 Study", username: "gburdell", post_date: Date(), tags: [Tag.homework, Tag.class_project, Tag.cs1999], description: "Lorem ipsum dolor sit amet, sed do eiusmod tempor quis nos vas de roma.")
     }
 }
