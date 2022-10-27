59011a7368c9a9d177bd9c1b56b5dd1a355d6d69
Nicholas Candello <nac5504@gmail.com>
2022-10-27 18:41:33 -0400

    created the class

---
+++ GTLink/FeedViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewModel.swift
+//  GTLink
+//
+//  Created by Nicholas Candello on 10/27/22.
+//
+
+import Foundation
+
+class FeedViewModel: ObservableObject {
+    
+//    Create a class in a new file in the Model folder called FeedViewModel.
+//
+//    Make sure it has an instance variable that stores some instance of the UserViewMode class. We need this to be able to handle user interactions with any post.
+//    This class will handle database operations related to projects in the feed.
+//    Functions include but are not limited to:
+//    sync(), fetches new projects and data regarding the feed from the database (Firestore).
+//    add(), adds new projects to feed (in the database). Also adds the project to the current user's list of projects.
+    
+    // fetches new projects and data regarding the feed from the database (Firestore).
+    func sync() {
+        
+    }
+    
+    
+    // adds new projects to feed (in the database). Also adds the project to the current user's list of projects.
+    func add() {
+        
+    }
+    
+    
+}
