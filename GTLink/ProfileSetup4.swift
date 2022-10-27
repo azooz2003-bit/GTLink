7a27f8fd38a0966908e5e03e23e09760e2d3bfa0
Ria Patel <ria2004.patel@gmail.com>
2022-10-27 18:35:15 -0400

    Edit Post

---
+++ GTLink/ProfileSetup4.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileSetup4.swift
+//  GTLink
+//
+//  Created by Ria Patel on 10/25/22.
+//
+
+import SwiftUI
+
+struct ProfileSetup4: View {
+    var body: some View {
+        VStack {
+            Text("Select a few of your interests.")
+        }
+    }
+}
+
+struct ProfileSetup4_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSetup4()
+    }
+}
