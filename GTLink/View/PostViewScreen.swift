99f1caf2389581d955e06abfcbd529f05c026b56
rishi-desai <rdesai82@gatech.edu>
2022-11-03 12:43:53 -0400

    implement tag view component that lists tags in a horizontal scroll view stack style

--- GTLink/View/PostViewScreen.swift
+++ GTLink/View/PostViewScreen.swift
@@ -7,50 +7,69 @@
 
 import SwiftUI
 
-struct Tag: Identifiable {
-    let id = UUID()
-    var name: String
-    var category: String
-    var color: Color
-}
+var user: String = "gburdell"
+var date: String = "10/11/22"
+var title: String = "AppDev: GTLink"
+var image: String = "xcode 1"
+var description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
-struct PostViewScreen: View {
-    var user: String = "gburdell"
-    var date: String = "10/11/22"
-    var title: String = "AppDev: GTLink"
-    var image: String = "xcode 1"
-    var description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    var tags = [
-        Tag(name: "Swift", category: "Language", color: .blue),
-        Tag(name: "iOS Club", category: "Course", color: .purple),
-        Tag(name: "Beginner", category: "Experience", color: .orange),
-        Tag(name: "Designer", category: "Role", color: .pink),
-        Tag(name: "UI/UX", category: "Role", color: .pink),
-        Tag(name: "Database", category: "Role", color: .pink),
+struct PostViewScreen: View  {
+    @State var tags: [Tag] = [
+        addTag(text: "iOS", fontSize: 16),
+        addTag(text: "Developer", fontSize: 16),
+        addTag(text: "Beginner", fontSize: 16),
+        addTag(text: "Swift", fontSize: 16),
+        addTag(text: "iOS Club", fontSize: 16),
+        addTag(text: "UI/UX", fontSize: 16)
+//        Tag(text: "iOS"),
+//        Tag(text: "Developer"),
+//        Tag(text: "Beginner"),
+//        Tag(text: "Swift"),
+//        Tag(text: "iOS Club"),
+//        Tag(text: "UI/UX")
     ]
     
     var body: some View {
         VStack {
+            titleCard()
+            descriptionView()
+            TagView(maxLimit: 150, tags: $tags)
+                .padding(.top, 5)
+            requestButton()
+            Spacer()
+        }
+    }
+}
+
+struct titleCard: View {
+    var body: some View {
+        VStack {
             Image(image)
                 .resizable()
-                .frame(width: 390, height: 180)
-                .cornerRadius(30)
+                .frame(width: UIScreen.main.bounds.width, height: 180)
             Text(title)
                 .fontWeight(.bold)
                 .font(.title)
                 .padding(5)
             HStack {
-                Label(user, systemImage: "person")
+                Label(user, systemImage: "person.fill")
                     .padding(.leading)
                     .font(.subheadline)
                     .foregroundColor(.secondary)
                 Spacer()
-                Label(date, systemImage: "calendar")
+                Label(date, systemImage: "calendar.circle.fill")
                     .padding(.trailing)
                     .font(.subheadline)
                     .foregroundColor(.secondary)
             }
             .padding(.bottom)
+        }
+    }
+}
+
+struct descriptionView: View {
+    var body: some View {
+        VStack {
             Text("Description")
                 .font(.title3)
                 .underline()
@@ -61,45 +80,26 @@ struct PostViewScreen: View {
                 .padding(.top, 5)
                 .padding([.leading, .trailing], 15)
                 .font(.callout)
-            Text("Tags")
-                .font(.title3)
-                .underline()
-                .padding(.top, 10)
-                .padding(.leading)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            ScrollView(.horizontal) {
-                HStack {
-                    ForEach(tags) { tag in
-                        Text(tag.name)
-                            .font(.subheadline)
-                            .padding([.top, .bottom], 10)
-                            .padding([.leading, .trailing], 20)
-                            .foregroundColor(.white)
-                            .background(tag.color)
-                            .clipShape(Rectangle())
-                            .cornerRadius(10)
-                            .fixedSize(horizontal: true, vertical: false)
-                        Spacer(minLength: 10)
-                    }
-                }
-                .padding(5)
-            }
-            Button {
-                print("Request Submitted!")
-            } label: {
-                Text("Send Request")
-                    .font(.title2)
-                    .padding([.top, .bottom], 15)
-                    .padding([.leading, .trailing], 55)
-                    .foregroundColor(.black)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
-            }
-            .padding(55)
-            Spacer()
         }
+    }
+}
+
+struct requestButton: View {
+    var body: some View {
+        Button {
+            print("Request Submitted!")
+        } label: {
+            Text("Send Request")
+                .font(.title2)
+                .padding([.top, .bottom], 15)
+                .padding([.leading, .trailing], 55)
+                .foregroundColor(.black)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+        }
+        .padding(10)
     }
 }
 
---
+++ GTLink/View/TagView.swift
@@ -0,0 +1,146 @@
+//
+//  TagView.swift
+//  GTLink
+//
+//  Created by rishi on 11/1/22.
+//
+
+import SwiftUI
+
+struct Tag: Identifiable, Hashable {
+    let id = UUID().uuidString
+    var text: String
+    var size: CGFloat = 0
+}
+
+struct TagView: View {
+    var maxLimit: Int
+    @Binding var tags: [Tag]
+    
+    var fontSize: CGFloat = 16
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Tags")
+                .font(.title3)
+                .underline()
+                .padding(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    // diplaying tags
+                    ForEach(getRows(), id: \.self) { rows in
+                        HStack(spacing: 6) {
+                            ForEach(rows) { row in
+                                RowView(tag: row)
+                            }
+                        }
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width - 30, alignment: .leading)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 100)
+            
+            // uncomment to see border of where the tags are displayed
+            /*
+            .background (
+                RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(.black)
+            )
+            */
+            .padding(10)
+        }
+//        .onChange(of: tags) { newValue in
+//        }
+    }
+    
+    @ViewBuilder
+    func RowView(tag: Tag) -> some View {
+        // applying same font size
+        // else size will vary
+        Text(tag.text)
+            .font(.system(size: fontSize))
+        // adding capsule
+            .padding(.horizontal, 14)
+            .padding(.vertical, 8)
+            .background (
+                // need to figure out how to classify specific tags by color and implement it here or may need to add parameter to each Tag instance
+                Capsule()
+                    .fill(.blue)
+            )
+            .foregroundColor(.white)
+            .lineLimit(1)
+        // deleting
+            .contentShape(Capsule())
+            .contextMenu {
+                Button("Delete") {
+                    tags.remove(at: getIndex(tag: tag))
+                }
+            }
+    }
+    
+    func getIndex(tag: Tag) -> Int {
+        let index = tags.firstIndex { currentTag in
+            return tag.id == currentTag.id
+        } ?? 0
+        return index
+    }
+    
+    // basic logic
+    // splitting array when it exceeds screen size
+    func getRows() -> [[Tag]] {
+        var rows: [[Tag]] = []
+        var currentRow: [Tag] = []
+        
+        // calculating text width
+        var totalWidth: CGFloat = 0
+        // if issues occurs with tags now showing properly or cutting off change 30 to 40
+        let screenWidth: CGFloat = UIScreen.main.bounds.width - 30
+        
+        
+        tags.forEach { tag in
+            // updating total width
+            // adding capsule size to total width with spacing
+            totalWidth += (tag.size + 40)
+            
+            if totalWidth > screenWidth {
+                // adding row in rows
+                // clearing out data
+                // checking for long string
+                totalWidth = (!currentRow.isEmpty || rows.isEmpty ? (tag.size + 40) : 0)
+                rows.append(currentRow)
+                currentRow.removeAll()
+                currentRow.append(tag)
+            } else {
+                currentRow.append(tag)
+            }
+        }
+        if !currentRow.isEmpty {
+            rows.append(currentRow)
+            currentRow.removeAll()
+        }
+        
+        return rows
+    }
+}
+
+struct TagView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostViewScreen()
+    }
+}
+
+// global addtag function
+func addTag(text: String, fontSize: CGFloat) -> Tag {
+    
+    // getting text size
+    let font = UIFont.systemFont(ofSize: fontSize)
+    let attributes = [NSAttributedString.Key.font: font]
+    let size = (text as NSString).size(withAttributes: attributes)
+    
+    return Tag(text: text, size: size.width)
+}
