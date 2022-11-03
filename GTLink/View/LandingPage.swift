17c498a6a0406b7cd09d8e44ce867cc229146880
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-11-03 14:32:44 -0400

    REVIEW: comments added regarding necessary changes.

--- GTLink/View/LandingPage.swift
+++ GTLink/View/LandingPage.swift
@@ -16,18 +16,21 @@ struct LandingPage: View {
             Spacer()
                 .frame(height:170)
             
+            // Use the exact icons as in Figma. Not emojis.
             Text("❖ Create/Join Project Teams")
                 .padding(10)
                 .offset(x: 0)
                 .foregroundColor(.black)
                 .font(.system(size: 20))
             
+            // same here
             Text("➠ Form Study Groups")
                 .padding(10)
                 .foregroundColor(.black)
                 .font(.system(size: 20))
                 .offset(x: -25)
             
+            // same here
             Text("↹ Connect with Students")
                 .padding(10)
                 .foregroundColor(.black)
@@ -40,11 +43,16 @@ struct LandingPage: View {
                     Image("GT")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
+                    
+                    // Good job with choosing an HStack. Now find a way to contain the image, text, arrow, with proper spacing.
                     Text("Georgia Tech SSO ➩  ")
                         .foregroundColor(.white)
                         .font(.custom(
                             "AmericanTypewriter",
                             fixedSize: 25))
+                // ^ NOTE: change the font so that it matches the design! OR instead of .custom(), have a look at .system(size:weight:design:) or some other variation. Don't use the arrow emoji as an icon!
+                    // If you go to Figma, you can "export" the specific arrow used as an SVG file. Saves you the hassle of finding a similar arrow.
+                    // Label component may be useful here
                 }
                 .background(Color.black)
                 .cornerRadius(15)
