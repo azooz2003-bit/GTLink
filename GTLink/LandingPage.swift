9f2c7a86a9e994512202491395e7c5cc99783253
Daniel Yu <dyu318@gatech.edu>
2022-11-06 16:03:57 -0500

    Draft 2.0

--- GTLink/LandingPage.swift
+++ GTLink/LandingPage.swift
@@ -10,44 +10,66 @@ import SwiftUI
 struct LandingPage: View {
     var body: some View {
         VStack {
+        
             Text("GT Link")
                 .font(.system(size: 60))
                 .padding(30)
             Spacer()
-                .frame(height:170)
-            
-            Text("❖ Create/Join Project Teams")
-                .padding(10)
-                .offset(x: 0)
-                .foregroundColor(.black)
-                .font(.system(size: 20))
-            
-            Text("➠ Form Study Groups")
-                .padding(10)
-                .foregroundColor(.black)
-                .font(.system(size: 20))
-                .offset(x: -25)
-            
-            Text("↹ Connect with Students")
-                .padding(10)
-                .foregroundColor(.black)
-                .font(.system(size: 20))
-                .offset(x: -12)
+                .frame(height:180)
+            HStack {
+                Image("iconOne")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                Text("Create/Join Project Teams")
+                    .padding(10)
+                    .offset(x: 0)
+                    .foregroundColor(.black)
+                    .font(.system(size: 20))
+            }
+            HStack {
+                Image("iconTwo")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .offset(x: -28)
+                
+                Text("Form Study Groups")
+                    .padding(10)
+                    .foregroundColor(.black)
+                    .font(.system(size: 20))
+                    .offset(x: -27)
+            }
+            HStack {
+                Image("iconThree")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .offset(x: -13)
+                Text("Connect with Students")
+                    .padding(10)
+                    .foregroundColor(.black)
+                    .font(.system(size: 20))
+                    .offset(x: -20)
+            }
             
             Spacer()
             NavigationLink(destination: FeedScreen()) {
                 HStack {
+                    Text("          ")
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
                     Image("GT")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                    Text("Georgia Tech SSO ➩  ")
+                        
+                    Text("Georgia Tech SSO")
                         .foregroundColor(.white)
-                        .font(.custom(
-                            "AmericanTypewriter",
-                            fixedSize: 25))
+                        .font(.system(size: 25))
+                    Image("Right")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Text("          ")
                 }
                 .background(Color.black)
-                .cornerRadius(15)
+                .cornerRadius(20)
                 .frame(height: 60)
                 .padding(-100)
             }
