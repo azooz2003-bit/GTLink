e41e3cc678ab6ec0d6d014d8b3815c6852f8d879
Nicholas Candello <nac5504@gmail.com>
2022-11-06 11:21:20 -0500

    added functionality to the tag buttons

--- GTLink/View/FeedFilterView.swift
+++ GTLink/View/FeedFilterView.swift
@@ -10,12 +10,13 @@ import Foundation
 
 struct FeedFilterView: View {
     
-    @State private var tagsSelected: [Tag] = []
+    @State private var tagsSelected: [Tag]
     @State private var projectType: String = "Project"
     
     init(tags: [Tag], projectType: String) {
         self.tagsSelected = tags
         self.projectType = projectType
+        
     }
     
     var body: some View {
@@ -31,19 +32,34 @@ struct FeedFilterView: View {
                     .font(.system(size: 20))
                 VStack {
                     ForEach(Tag.allCases, id: \.hashValue) { tag in
-                        Text(tag.rawValue).padding(10).background(tag.color).cornerRadius(15)
+                        Button(action: {
+                            buttonClicked(tag: tag)
+                        }, label: { Text(tag.rawValue).padding(10).background(tagsSelected.contains(tag) ? tag.color : Color.white).cornerRadius(15).foregroundColor( tagsSelected.contains(tag) ? Color.white : Color(red: 121/255, green: 121/255, blue: 121/255))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .stroke(tagsSelected.contains(tag) ? Color.clear : tag.color)
+                                )
+                        })
                     }
-                }.foregroundColor(.white)
+                }
                 .font(.system(size: 16))
-            }
+            }.frame(width: UIScreen.main.bounds.size.width)
             
         }
     }
-    
+    func buttonClicked(tag: Tag) {
+        if(tagsSelected.contains(tag)) {
+            tagsSelected.removeAll { thisTag in
+                thisTag == tag
+            }
+        } else {
+            tagsSelected.append(tag)
+        }
+    }
 }
 
 struct FeedFilterView_Previews: PreviewProvider {
     static var previews: some View {
-        FeedFilterView(tags: [Tag.homework, Tag.cs1301, Tag.cs1999], projectType: "Project")
+        FeedFilterView(tags: [Tag.homework, Tag.cs1301, Tag.cs1999, Tag.ios, Tag.designer], projectType: "Project")
     }
 }
