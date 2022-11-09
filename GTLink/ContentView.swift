9e1b478303eb8c448fd89d9346f57dbed573cdb5
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2022-11-08 21:40:33 -0500

    boilerplate files added

---
+++ GTLink/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GTLink
+//
+//  Created by Abdulaziz Albahar on 11/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
