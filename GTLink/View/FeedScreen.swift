c0df3be68d98cb4f42374a65f68278f497e5bb02
Johnny Sanchez <johnnysanchez730@icloud.com>
2022-11-09 19:08:32 -0500

    Added ScrollView to Requests Screen

--- GTLink/View/FeedScreen.swift
+++ GTLink/View/FeedScreen.swift
@@ -9,30 +9,35 @@ import SwiftUI
 
 struct FeedScreen: View {
     var body: some View {
-        VStack {
-            ScrollView {
-                Text("Pending Requests")
-                    .font(.system(size: 25))
-                    .bold()
-                    .underline()
-                Spacer()
-                
-                PendingRequestCard(post_picture: Image("penguin"), status: "Wants to work with you on", pname: "AppDev: GTLink")
-                
-                Spacer()
-                
-                Text("Accepted Requests")
-                    .font(.system(size: 25))
-                    .bold()
-                    .underline()
-        
-                AcceptedRequestCard(post_picture: Image("penguin"), status: "George Burdell accepted your request to work on", pname: "WebDev: GT Dashboard")
-                
-                AcceptedRequestCard(post_picture: Image("penguin"), status: "You accepted George Burdell's request to work on", pname: "CS 1999: Exam 1 Study")
-            }
+        ScrollView {
+            VStack {
+                    Text("Pending Requests")
+                        .font(.system(size: 25))
+                        .bold()
+                        .underline()
+                    Spacer()
+                    
+                    PendingRequestCard(post_picture: Image("penguin"), status: "Wants to work with you on", pname: "AppDev: GTLink")
+                    
+                    Spacer()
+                    
+                    Text("Accepted Requests")
+                        .font(.system(size: 25))
+                        .bold()
+                        .underline()
             
+                    Spacer()
+                
+                    AcceptedRequestCard(post_picture: Image("penguin"), status: "George Burdell accepted your request to work on", pname: "WebDev: GT Dashboard")
+                    
+                    Spacer()
+                
+                    AcceptedRequestCard(post_picture: Image("penguin"), status: "You accepted George Burdell's request to work on", pname: "CS 1999: Exam 1 Study")
+                
+                    Spacer()
+                
+            }
         }
-        
     }
 }
 
