3ffe08e8507c8e90535108adbbf77986142313e0
Johnny Sanchez <johnnysanchez730@icloud.com>
2022-11-09 19:04:44 -0500

    Implemented requests screen

--- GTLink/View/FeedScreen.swift
+++ GTLink/View/FeedScreen.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct FeedScreen: View {
     var body: some View {
-        Text("Feed")
+        VStack {
+            ScrollView {
+                Text("Pending Requests")
+                    .font(.system(size: 25))
+                    .bold()
+                    .underline()
+                Spacer()
+                
+                PendingRequestCard(post_picture: Image("penguin"), status: "Wants to work with you on", pname: "AppDev: GTLink")
+                
+                Spacer()
+                
+                Text("Accepted Requests")
+                    .font(.system(size: 25))
+                    .bold()
+                    .underline()
+        
+                AcceptedRequestCard(post_picture: Image("penguin"), status: "George Burdell accepted your request to work on", pname: "WebDev: GT Dashboard")
+                
+                AcceptedRequestCard(post_picture: Image("penguin"), status: "You accepted George Burdell's request to work on", pname: "CS 1999: Exam 1 Study")
+            }
+            
+        }
+        
     }
 }
 
