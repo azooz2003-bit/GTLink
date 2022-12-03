424067dcdcbaf52ebe53e67ee4bcc1e85c33aa21
rishi-desai <rdesai82@gatech.edu>
2022-12-03 14:46:51 -0500

    fixed error in requestToJoinProject func

--- GTLink/New Group/Model/Post.swift
+++ GTLink/New Group/Model/Post.swift
@@ -15,16 +15,17 @@ struct Post {
     var postingID: String
     var title: String
     var image: String
-    let owner: String // UserID/uid of whoever created the post
-    let date: Date // The Date the Post was created
+    var owner: String // UserID/uid of whoever created the post
+    var date: Date // The Date the Post was created
     var description: String
     var tags: [String: Bool] // All Tags and whether or not they are used
     var receivedRequests: [String: [String: Bool]]
-    let isProject: Bool
-    let isStudy: Bool
+    var isProject: Bool
+    var isStudy: Bool
     var members: [String] // All the uid/User ID's of the people associated with the Post
     
-    init(title: String, image: String, owner: String, date: Date, description: String, tags: [String : Bool], receivedRequests: [String: [String: Bool]], isProject: Bool, isStudy: Bool, members: String) {
+    init(postingID: String, title: String, image: String, owner: String, date: Date, description: String, tags: [String : Bool], receivedRequests: [String: [String: Bool]], isProject: Bool, isStudy: Bool, members: String) {
+        self.postingID = postingID
         self.title = title
         self.image = image
         self.owner = owner
